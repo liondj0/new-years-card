ca89c93ee9f784eb12ee1bdcc7e409a4c71d952b
liondj0 <liondj007@gmail.com>
2018-12-31 19:47:41 +0100

    hehehe

---
+++ js/main.js
@@ -0,0 +1,9 @@
+particlesJS.load('particles', 'particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles2', 'particles.1.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles3', 'particles.2.json', function () {
+    console.log('callback - particles.js config loaded');
+});
